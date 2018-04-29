57a7ffbdb291d49fa3f4f36c57916e5a6228bded
Jason Dees <jhdees@gmail.com>
2018-04-29 12:33:04 -0400

    it's a box instead of a ship now

--- City Scape/Visibles/CityNode.swift
+++ City Scape/Visibles/CityNode.swift
@@ -11,14 +11,14 @@ import ARKit
 
 class CityNode : SCNNode {
     
-    var shipNode : SCNNode!
+    var cityNode : SCNNode!
     var planeNode : SCNNode!
     public private(set) var hasRendered: Bool =  false
     public var isMain : Bool = false {
         didSet{
-            let color = isMain ? UIColor.gray : UIColor.green
+            let color = isMain ? UIColor.red : UIColor.green
             setPlaneNodeColor(color: color)
-            shipNode.isHidden = !isMain
+            cityNode.isHidden = !isMain
         }
     };
     public var planeAnchor: ARPlaneAnchor!{
@@ -39,31 +39,40 @@ class CityNode : SCNNode {
     
     private func setup(){
         
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        shipNode = scene.rootNode
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        cityNode = scene.rootNode
+        let box = SCNBox.init(width: 1.0, height: 2.0, length: 1.0, chamferRadius: 0.0)
+        box.firstMaterial?.diffuse.contents = UIColor.blue
+        box.firstMaterial?.metalness.contents = UIColor.cyan
+        cityNode = SCNNode()
+        cityNode.geometry = box
+        cityNode.opacity = 0.75
+        
         planeNode = SCNNode()
         planeNode.name = "plane"
         planeNode?.eulerAngles.x = -.pi / 2
         planeNode?.opacity = 0.25
         
-        self.addChildNode(shipNode)
+        self.addChildNode(cityNode)
         self.addChildNode(planeNode)
         let constraint = SCNTransformConstraint(inWorldSpace: false, with:{
             node, transformMatrix in
             
             let width = self.nodeWidth(node: self.planeNode)
             let height = self.nodeHeight(node: self.planeNode)
-            let shipWidth = self.nodeWidth(node: node)
-            let shipHeight = self.nodeHeight(node: node)
-            var scale = width/shipWidth
+            let cityWidth = self.nodeWidth(node: node)
+            let cityHeight = self.nodeHeight(node: node)
+            var scale = (width - 0.1)/cityWidth
             if(width > height){
-                scale = height/shipHeight
+                scale = (height - 0.1)/cityHeight
             }
+            node.position = SCNVector3(x: self.planeNode.position.x,
+                                       y: cityHeight/2 * scale,
+                                       z: self.planeNode.position.z)
             node.scale = SCNVector3(scale, scale, scale)
-            node.position = self.planeNode.position
             return node.transform
             })
-        shipNode.constraints = [constraint]
+        cityNode.constraints = [constraint]
     }
     
     private func setPosition(nextPlaneAnchor: ARPlaneAnchor) {
